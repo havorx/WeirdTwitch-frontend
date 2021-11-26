2e02b9fdfd9306c67d1851d8d29945dc4c4f4bc9
havorx <55874008+jazzkatxlemon@users.noreply.github.com>
2021-11-27 00:48:26 +0700

    fix login + navbar + createStream

--- src/pages/CreateStream/CreateStream.js
+++ src/pages/CreateStream/CreateStream.js
@@ -1,45 +1,65 @@
-import React from 'react'
-import { Container, Form, Row, Col, Button } from 'react-bootstrap'
+import React, {useEffect, useState} from 'react'
+import {Container, Form, Row, Col, Button} from 'react-bootstrap'
+import {myAxios} from "../../utils/AxiosSetup";
 
 export default function CreateStream() {
+    const [categories, setCategories] = useState([]);
+
+    function getCategory() {
+        myAxios.get('/category/get-category').then(response => {
+            const data = response.data;
+            const categoryList = data.map(element => element.categoryName);
+            setCategories(categoryList);
+        });
+    }
+
+    useEffect(() => {
+        getCategory();
+    }, []);
+
     return (
         <article>
             <Container className="mt-5">
                 <h5>Create Room</h5>
                 <div className="mt-5 d-flex flex-column justify-content-center align-items-center">
-                    <Form style={{ width: '500px' }}>
+                    <Form style={{width: '500px'}}>
                         <Row className="mb-5">
                             <Form.Group as={Col} md="6" controlId="validationCustom01">
                                 <Form.Label>Room's name</Form.Label>
                                 <Form.Control
                                     required
                                     type="text"
-                                    placeholder="Room's name"
+                                    placeholder="Choose a name"
                                 />
                             </Form.Group>
                             <Form.Group as={Col} md="6" controlId="validationCustom01">
                                 <Form.Label>Category</Form.Label>
-                                <Form.Select required >
-                                    <option>Large select</option>
-                                    <option>Large select</option>
-                                    <option>Large select</option>
+                                <Form.Select required>
+                                    <option>Select a category</option>
+                                    {categories.map(element => (
+                                        <option>{element}</option>
+                                    ))}
                                 </Form.Select>
                             </Form.Group>
                         </Row>
-                        <Row className="mb-5 px-2">
-                            <Form.Control
-                                as="textarea"
-                                placeholder="Description"
-                                style={{ minHeight: '100px' }}
-                            />
-                        </Row>
-
+                        <Form.Group>
+                            <Form.Label>Description</Form.Label>
+                            <Row className="mb-5 px-2">
+                                <Form.Control
+                                    as="textarea"
+                                    placeholder="Enter description"
+                                    style={{minHeight: '100px'}}
+                                />
+                            </Row>
+                        </Form.Group>
                         <Row className="mb-3 d-flex flex-row">
-                            <Form.Group className="d-flex justify-content-end" as={Col} md="6" controlId="validationCustom01">
-                                <Button style={{ width: '100px' }} className="me-2 buttonOutlined" >Cancel</Button>
+                            <Form.Group className="d-flex justify-content-end" as={Col} md="6"
+                                        controlId="validationCustom01">
+                                <Button style={{width: '100px'}} className="me-2 buttonOutlined">Cancel</Button>
                             </Form.Group>
                             <Form.Group as={Col} md="6" controlId="validationCustom01">
-                                <Button style={{ width: '100px' }} className="me-2 buttonFilledSecondary" variant="outline-none">Create</Button>
+                                <Button style={{width: '100px'}} className="me-2 buttonFilledSecondary"
+                                        variant="outline-none">Create</Button>
                             </Form.Group>
                         </Row>
                     </Form>
