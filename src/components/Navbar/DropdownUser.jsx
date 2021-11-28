e11df2901502f3589e602df7ce6ae7ecc80291b9
havorx <55874008+jazzkatxlemon@users.noreply.github.com>
2021-11-28 17:55:34 +0700

    Update DropdownUser.jsx

--- src/components/Navbar/DropdownUser.jsx
+++ src/components/Navbar/DropdownUser.jsx
@@ -1,17 +1,16 @@
-import { useContext } from 'react';
-import { Dropdown, DropdownButton, Image } from 'react-bootstrap';
+import {useContext} from 'react';
+import {Dropdown, DropdownButton, Image} from 'react-bootstrap';
 import {
-    User as UserIcon,
-    Settings as SettingIcon,
-    LogOut as LogOutIcon,
-    Airplay as AirplayIcon
+  User as UserIcon,
+  Settings as SettingIcon,
+  LogOut as LogOutIcon,
 } from 'react-feather';
 import image from '../../assets/03-glitch.jpg';
 import {myAxios} from '../../utils/AxiosSetup';
 import {UserContext} from '../../context/userContext.tsx';
-import { Link } from 'react-router-dom';
+
 export default function DropdownUser() {
-    const [userContext, setUserContext] = useContext(UserContext);
+  const [userContext, setUserContext] = useContext(UserContext);
 
   const logoutHandler = () => {
     myAxios.get('/auth/logout').then(async response => {
@@ -22,25 +21,9 @@ export default function DropdownUser() {
         setUserContext(oldValues => {
           return {...oldValues, token: null, username: null, isAdmin: null};
         });
-    };
-
-    return (
-        <DropdownButton menuVariant="dark" variant="secondary" align="end"
-            title={<UserIcon />}
-            id="dropdown-menu-align-end">
-            <div className="px-1 py-2">
-                <div className="d-flex align-items-center"
-                    style={{ padding: '0 16px' }}>
-                    <Image style={{
-                        height: '40px',
-                        width: '40px',
-                        borderRadius: '100%',
-                        marginRight: '10px',
-                    }}
-                        src={image} />
-                    <b>{userContext.username}</b>
-                </div>
-                <br />
+      }
+    });
+  };
 
   return (
       <DropdownButton menuVariant="dark" variant="secondary" align="end"
